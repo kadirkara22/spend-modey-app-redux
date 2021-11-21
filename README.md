1f5b191aed1e73af8e2f21b28e792c98c0292e6f
kadirkara22 <kadirkar2204@gmail.com>
2021-11-21 17:27:09 +0300

    README dosyası düzenlendi

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spend-modey-app-redux
+
+## https://spendmoneybillgates.netlify.app/
+
+![alt text](https://github.com/kadirkara22/NoteApp-with-redux/blob/main/src/image/notesOdev.PNG)
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./header.css"
 const Header = () => {
     return (
-        <div className="header">
+        <div className="header" style={{ height: "330px" }}>
             <img src="https://neal.fun/spend/billgates.jpg" alt="bill" className="bilgates"></img>
             <span className="header_title"> Spend Bill Gates' Money</span>
         </div>
